48146b01c3953e7f025162a13c95e4ac4509c074
Alex Raphael Abalos <nyshikastormix@gmail.com>
2023-11-06 13:42:54 +0800

    UI fixes 2

--- resources/views/tags_layout/tags_list.blade.php
+++ resources/views/tags_layout/tags_list.blade.php
@@ -14,8 +14,8 @@
 
 <div>
 <form style="margin:auto;max-width:300px">
-    <input type="search" class="form-control mr-sm-2" placeholder="Search Books" name="search"  value="{{ request('search') }}">
-    <input class="btn btn-primary my-2 my-sm-0" type="submit" value="Search">
+    <input type="text" class="form-control mr-sm-2" placeholder="Search Books" name="search"  value="{{ request('search') }}">
+    <input class="button btn-primary my-2 my-sm-0" type="submit" value="Search">
 </form>
 </div>
 
@@ -189,6 +189,7 @@
 
 @empty
   <li class="list-group-item list-group-item-danger">Entry not found</li>  
+  
 @endforelse
 
 
@@ -289,14 +290,20 @@
 @endforelse
 @endif
 
-
-
-
 </table>
 <div class="d-flex">
     <div class="mx-auto">
       <?php echo $tags->render(); ?>
     </div>
 </div>
+
+<style> 
+form { 
+  display: flex; 
+}
+input[type=text] 
+{ flex-grow: 1; 
+}
+</style>
 
 @endsection
--- resources/views/users_layout/users_list.blade.php
+++ resources/views/users_layout/users_list.blade.php
@@ -14,8 +14,8 @@
 
 <div>
 <form style="margin:auto;max-width:300px">
-    <input type="search" class="form-control mr-sm-2" placeholder="Search Users" name="search"  value="{{ request('search') }}">
-    <input class="btn btn-primary my-2 my-sm-0" type="submit" value="Search">
+    <input type="text" class="form-control mr-sm-2" placeholder="Search Users" name="search"  value="{{ request('search') }}">
+    <input class="button btn-primary my-2 my-sm-0" type="submit" value="Search">
 </form>
 </div>
 
@@ -91,4 +91,13 @@
 <?php echo $users->render(); ?>
   </div>
 </div>
+
+<style> 
+form { 
+  display: flex; 
+}
+input[type=text] 
+{ flex-grow: 1; 
+}
+</style>
 @endsection
